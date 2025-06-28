b29180d9f83d69c6c04c3a5617206833ec2f25e0
JI000000 <jipengfei0806@gmail.com>
2025-06-29 01:01:43 +0800

    fix: switch to vercel edge runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gemma-3n.net',
-  output: 'server',
+  output: 'hybrid',
   integrations: [tailwind(), sitemap(), vercel()]
 }); 
